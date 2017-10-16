ba4ba4b6986a471278d1c4fca2a6526a0b27193e
amboz <abozigian@gmail.com>
2017-10-16 13:18:22 -0700

    Specs passed

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,19 @@
 class Grub {
+  constructor () {
+    this.age = 0
+    this.color = 'pink'
+    this.food = 'jelly'
+  }
+  eat () {
+    return `Mmmmmmmmm ${this.food}`
+  }
 };
+
+// This assignment must be written with ES6 classes by using the
+// new class, extends, and super keywords
+
+// Create a Grub class, in ES6 style, with:
+// an age property that is set to 0
+// a color property that is set to pink
+// a food property that is set to jelly
+// an eat method that returns 'Mmmmmmmmm jelly'
